7d707bf47c4ba3f6a6640f5be57040271bfc130c
Mya <myaskylar14@gmail.com>
2021-07-26 13:18:11 +0100

    done exe D ✔

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,32 @@
-function todoList(todos) {
-  // Write your code here...
+function todoList(toDos) {
+  let content = document.querySelector("#content"); //<---------target content
+  let ul = document.createElement("ul"); //<------create <ul></ul>
+
+  toDos.forEach((list) => {
+    //<-----------iterate array
+    let li = document.createElement("li"); //<----- create <li> tag for each element
+    li.innerText = list.todo;
+    li.addEventListener("click", () => {
+      //<----- if click each <li>
+      if (li.style.textDecoration === "line-through") {
+        //<----- if <li style="text-decoration: line-through;">
+        li.removeAttribute("style"); //<----- remove style attribute
+      } else {
+        li.style.textDecoration = "line-through"; //<----- if <li></li> add style attribute
+      }
+    });
+    ul.appendChild(li); //<--- <ul><li></li></ul> assign <li> inside <ul>
+  });
+
+  content.append(ul);
+  content.className = "textAlign"; //<---- add css style (optional)
 }
 
-const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+const toDos = [
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
-todoList(todos);
+todoList(toDos);
