51224d7b5ad53ce122c1543e044b2fc4a7c063d3
Mya <myaskylar14@gmail.com>
2021-07-26 13:12:20 +0100

    done exe B ✔

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,14 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfShoppingList) {
+  let content = document.querySelector("#content"); //<------- target the div element to assign ul
+  let createUl = document.createElement("ul"); //<----------- create <ul></ul>
+  arrayOfShoppingList.forEach((product) => {
+    let createList = document.createElement("li"); //<---------- create list
+    createList.innerText = product;
+    createUl.appendChild(createList); //<------------ child element in ul
+  });
+
+  content.append(createUl);
+  content.className = "textAlign"; //<---- add css style (optional)
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
